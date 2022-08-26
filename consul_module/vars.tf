893c940c35cc2d620519a791948773a71513304d
Joao Pedro Ceragioli Correa <dodo.jpgta@gmail.com>
2022-08-25 23:09:47 -0300

    Section 2 completed

---
+++ consul_module/vars.tf
@@ -0,0 +1,9 @@
+variable "AWS_REGION" {
+    default = "us-east-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+    default = "/home/p00p/.ssh/ec2_terraform"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+    default = "/home/p00p/.ssh/ec2_terraform.pub"
+}
